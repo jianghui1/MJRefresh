bad53f90e1c1cdedceae54bacb4b38c146a28e01
jzh <jiangzenghui005@163.com>
2021-07-19 11:01:17 +0800

    去掉 UIScrollView+MJExtension 中 initialize 方法的实现，防止与其他第三方库发生冲突。

--- MJRefresh/UIScrollView+MJExtension.m
+++ MJRefresh/UIScrollView+MJExtension.m
@@ -14,22 +14,22 @@
 
 @implementation UIScrollView (MJExtension)
 
-static BOOL respondsToAdjustedContentInset_;
-
-+ (void)initialize
-{
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        respondsToAdjustedContentInset_ = [self instancesRespondToSelector:@selector(adjustedContentInset)];
-    });
-}
+//static BOOL respondsToAdjustedContentInset_;
+//
+//+ (void)initialize
+//{
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        respondsToAdjustedContentInset_ = [self instancesRespondToSelector:@selector(adjustedContentInset)];
+//    });
+//}
 
 - (UIEdgeInsets)mj_inset
 {
 #ifdef __IPHONE_11_0
-    if (respondsToAdjustedContentInset_) {
+//    if (respondsToAdjustedContentInset_) {
         return self.adjustedContentInset;
-    }
+//    }
 #endif
     return self.contentInset;
 }
@@ -39,9 +39,9 @@ static BOOL respondsToAdjustedContentInset_;
     UIEdgeInsets inset = self.contentInset;
     inset.top = mj_insetT;
 #ifdef __IPHONE_11_0
-    if (respondsToAdjustedContentInset_) {
+//    if (respondsToAdjustedContentInset_) {
         inset.top -= (self.adjustedContentInset.top - self.contentInset.top);
-    }
+//    }
 #endif
     self.contentInset = inset;
 }
@@ -56,9 +56,9 @@ static BOOL respondsToAdjustedContentInset_;
     UIEdgeInsets inset = self.contentInset;
     inset.bottom = mj_insetB;
 #ifdef __IPHONE_11_0
-    if (respondsToAdjustedContentInset_) {
+//    if (respondsToAdjustedContentInset_) {
         inset.bottom -= (self.adjustedContentInset.bottom - self.contentInset.bottom);
-    }
+//    }
 #endif
     self.contentInset = inset;
 }
@@ -73,9 +73,9 @@ static BOOL respondsToAdjustedContentInset_;
     UIEdgeInsets inset = self.contentInset;
     inset.left = mj_insetL;
 #ifdef __IPHONE_11_0
-    if (respondsToAdjustedContentInset_) {
+//    if (respondsToAdjustedContentInset_) {
         inset.left -= (self.adjustedContentInset.left - self.contentInset.left);
-    }
+//    }
 #endif
     self.contentInset = inset;
 }
@@ -90,9 +90,9 @@ static BOOL respondsToAdjustedContentInset_;
     UIEdgeInsets inset = self.contentInset;
     inset.right = mj_insetR;
 #ifdef __IPHONE_11_0
-    if (respondsToAdjustedContentInset_) {
+//    if (respondsToAdjustedContentInset_) {
         inset.right -= (self.adjustedContentInset.right - self.contentInset.right);
-    }
+//    }
 #endif
     self.contentInset = inset;
 }
